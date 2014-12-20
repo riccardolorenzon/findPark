6f4f0762e93a1b4c569bcf1d03c0e4449931ca4b
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-20 15:59:51 +0100

    ss

--- findPark/urls.py
+++ findPark/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    utl
 
 )
