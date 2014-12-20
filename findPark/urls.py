355dce064bc0a1f96c4aaf5a55389b3254e934ef
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-20 15:48:25 +0100

    hello

--- findPark/urls.py
+++ findPark/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import findParkBackend.views
+import findParkBackend.views as views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^/$', )
+    url(r'^/$', views.index)
 )
