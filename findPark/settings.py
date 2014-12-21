3d32cfed3f9530c10ca4d14cde49df334e281f9a
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-21 13:19:45 +0100

    added django rest framework

--- findPark/settings.py
+++ findPark/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'findParkBackend',
-    'gunicorn'
+    'gunicorn',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
--- findParkBackend/models.py
+++ findParkBackend/models.py
@@ -1,3 +1,51 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+# represents a parking spot on the state <state> starting from <laststatechange>
+class parkingspot(models.Model):
+    state = models.CharField(max_length=255)
+    laststatechange = models.DateTimeField()
+    #spot localization
+    poly = models.PolygonField()
+    objects = models.GeoManager()
+    #parking area
+    area = models.ForeignKey(parkingarea)
+    #transport type
+    transportcode = models.ForeignKey(transportclass)
+
+# represents the parking spot history, one object is created for each parking vehicle
+class parkingspot(models.Model):
+    state = models.CharField(max_length=255)
+    ready = models.DateTimeField()
+    occupied = models.DateTimeField()
+    #spot localization
+    poly = models.PolygonField()
+    objects = models.GeoManager()
+    #parking area
+    area = models.ForeignKey(parkingarea)
+    #transport type
+    transportcode = models.ForeignKey(transportclass)
+
+# represents a delimited area of the city/town
+class parkingarea(models.Model):
+    code = models.CharField(max_length=255)
+    description = models.TextField()
+
+# represents the class of the transport vehicle(a bike parking spot occupies less space than a car parking spot)
+class transportclass(models.Model):
+    code = models.CharField(max_length=255)
+    description = models.TextField()
+
+# represent the driver user, this entity
+class appuser(models.Mode):
+    code = models.CharField(max_length=255)
+    models.ManyToManyField(parkingspot, blank=True)
+    models.ManyToManyField(transportclass, blank=True)
+    #current localization
+    poly = models.PolygonField()
+    objects = models.GeoManager()
+
+# represents a passenger
+class passenger(models.Model):
+    code = models.CharField(max_length=255)
+    models.ForeignKey(appuser)
+
