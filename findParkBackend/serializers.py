6822d748e958d194c1e0f8158423fdcebd49068a
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-24 21:08:51 +0100

    added parking spots, parking area REST endpoints

--- findParkBackend/serializers.py
+++ findParkBackend/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from models import parkingspot, parkingarea, transportclass
 
 class parkingspotserializer(serializers.HyperlinkedModelSerializer):
-    area = serializers.HyperlinkedRelatedField(queryset = parkingarea.objects.all(), view_name='area-detail', format='json')
+    area = serializers.HyperlinkedRelatedField(queryset = parkingarea.objects.all(), view_name='parkingarea-detail', format='json')
     class Meta:
         model = parkingspot
         fields = ('state', 'laststatechange', 'latitude', 'longitude', 'area', 'transportcode')
