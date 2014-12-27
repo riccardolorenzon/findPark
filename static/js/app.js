500a5de400a82c0d873b412276af89f1054835b3
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-28 00:59:44 +0100

    fixed app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ findparkApp.controller('mapCtrl', function ($scope, $http) {
   $scope.map = { center: { latitude: 45.6702345, longitude: 12.2350815 }, zoom: 10 };
   $http.get('/proxy/gmapsdirections/srirangam/trichy')
       .success(function(data, status) {
-            
+                  
         })
       .error(function(data, status, headers, config) {
             $scope.restData = "errore nel ricevimento dati json ";
