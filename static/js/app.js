3eaf5e0b91f89df75077eb854a4adfe512e15d32
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-30 15:50:36 +0100

    added moving markers

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 var findparkApp = angular.module('findPark', ['uiGmapgoogle-maps']);
 
-findparkApp.controller('mapCtrl', function ($scope, $http, $log, $timeout) {
+findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval) {
   $scope.map = {center: {latitude: 45.6702345, longitude: 12.2350815 }, zoom: 12, bounds: {}};
   // get directions from <origin> to <destination>
   // TODO this call is necessary for each point in the simulation
@@ -14,6 +14,7 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $timeout) {
         latitude: 45.6702345,
         longitude: 12.2350815
       },
+      icon: "http://maps.google.com/intl/en_us/mapfiles/ms/micons/yellow.png",
       options: { draggable: false },
       events: {
         dragend: function (marker, eventName, args) {
@@ -40,6 +41,7 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $timeout) {
         longitude: 12.2350815
       },
       options: { draggable: false },
+      icon: "http://maps.google.com/intl/en_us/mapfiles/ms/micons/red.png",
       events: {
         dragend: function (marker, eventName, args) {
           $log.log('marker dragend');
@@ -65,6 +67,7 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $timeout) {
         longitude: 12.2350815
       },
       options: { draggable: false },
+      icon: "http://maps.google.com/intl/en_us/mapfiles/ms/micons/green.png",
       events: {
         dragend: function (marker, eventName, args) {
           $log.log('marker dragend');
@@ -84,112 +87,123 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $timeout) {
     };
 
 
-  function fnsuccess(data, status, id){
-      $scope.polylines[id-1] = {};
-      $scope.polylines[id-1].id = 1;
-      $scope.polylines[id-1].stroke =  {
+  function fnsuccess(data, status, id, step, marker){
+
+        $scope.polylines[id-1] = {};
+        $scope.polylines[id-1].id = 1;
+        $scope.polylines[id-1].stroke =  {
         color: '#60' + id + '0FB',
         weight: 2
-      };
-      $scope.polylines[id-1].visible = true;
-      $scope.polylines[id-1].path = [];
-        var jsonObj = JSON.parse(JSON.stringify(data));
+        };
+        $scope.polylines[id-1].visible = true;
+        $scope.polylines[id-1].path = [];
+        var jsonObj = data; //JSON.parse(JSON.stringify(data));
         obj.id = id;
         obj.stroke =  {
             color: '#60' + id + '0FB',
             weight: 2
         };
         obj.visible = true;
-        for (var rIndex = 0; rIndex < jsonObj.routes.length; rIndex++) {
-            var route = jsonObj.routes[rIndex];
-            for(var lIndex = 0; lIndex < route.legs.length; lIndex ++){
-                var leg = route.legs[lIndex];
-                for(var sIndex = 0; sIndex < leg.steps.length; sIndex++){
-                    var step = leg.steps[sIndex];
-                    if (typeof obj.path == "undefined")
-                    {
-                        obj.path = [];
-                    }
-                    start_location = {};
-                    start_location.latitude = step.start_location.lat;
-                    start_location.longitude = step.start_location.lng;
 
-                    end_location = {};
-                    end_location.latitude = step.end_location.lat;
-                    end_location.longitude = step.end_location.lng;
-                    /*
-                    obj.path.push(start_location);
-                    obj.path.push(end_location);
-                    */
-                    $scope.paths[id-1] = obj;
-                    $scope.paths[id-1].path.push(start_location);
-                    $scope.paths[id-1].path.push(end_location);
+        var step =  jsonObj.routes[0].legs[0].steps[step];
 
-                    //$scope.polylines[id-1].path.push(start_location);
-                    //$scope.polylines[id-1].path.push(end_location);
+        end_location = {};
+        end_location.latitude = step.end_location.lat;
+        end_location.longitude = step.end_location.lng;
 
-                    $scope.alice.coords.latitude = end_location.latitude;
-                    $scope.alice.coords.longitude = end_location.longitude;
-                }
-                 var step = leg.steps[leg.steps.length -1];
-                 end_location = {};
-                 end_location.latitude = step.end_location.lat;
-                 end_location.longitude = step.end_location.lng;
-
-                 //$scope.alice.coords.latitude = end_location.latitude;
-                 //$scope.alice.coords.longitude = end_location.longitude;
-            }
-        }
-
-        /*for (var i = 0; i < $scope.paths.length; i++) {
-                var p = $scope.paths[i];
-                for (var j = 0; j < p.path.length; j++) {
-                    xpath = p.path[j];
-                    $scope.polylines[id-1].path.push(xpath);
-                }
-         }*/
+        marker.coords.latitude = end_location.latitude;
+        marker.coords.longitude = end_location.longitude;
   }
 
   function sleep(milliseconds) {
       var start = new Date().getTime();
       for (var i = 0; i < 1e7; i++) {
-          console.log('working');
         if ((new Date().getTime() - start) > milliseconds){
           break;
         }
       }
     }
 
+  checkStep = function(jsonObj, step)
+  {
+    if (typeof  jsonObj.routes == "undefined")
+        return 0;
+    if (step == jsonObj.routes[0].legs[0].steps.length - 1)
+        {
+            return 0;
+        }
+    return step;
+  };
+
+
   $scope.jsonAlice = {};
+  $scope.stepAlice = 0;
 
-  $scope.startSimulation = function() {
-      var boh;
-      var boh2;
-      $http.get('/proxy/gmapsdirections/Silea+TV/Duomo,+Treviso,+TV/')
-          .success(function (data, status) {
-            $scope.jsonAlice = JSON.parse(JSON.stringify(data));
-            fnsuccess(data, status, 1);
-          })
-          .error(function (data, status, headers, config) {
-              $scope.restData = "errore nel ricevimento dati json ";
-          });
-      //TODO timeout function with increasing step number
+  $scope.jsonBob = {};
+  $scope.stepBob = 0;
+
+  $scope.jsonChuck = {};
+  $scope.stepChuck = 0;
+
+  $scope.simulation = function() {
+      $scope.stepAlice = checkStep($scope.jsonAlice, $scope.stepAlice);
+      //alice
+      if($scope.stepAlice == 0) {
+          $http.get('/proxy/gmapsdirections/Silea+TV/Duomo,+Treviso,+TV/')
+              .success(function (data, status) {
+                  $scope.jsonAlice = JSON.parse(JSON.stringify(data));
+                  fnsuccess($scope.jsonAlice, status, 1, $scope.stepAlice, $scope.alice);
+              })
+              .error(function (data, status, headers, config) {
+                  $scope.restData = "errore nel ricevimento dati json ";
+              });
+      }
+      else
+      {
+          fnsuccess($scope.jsonAlice, status, 1, $scope.stepAlice, $scope.alice);
+      }
+      $scope.stepAlice +=1;
+
+      //bob
+
+      $scope.stepBob = checkStep($scope.jsonBob, $scope.stepBob);
+      if($scope.stepBob == 0) {
+          $http.get('/proxy/gmapsdirections/Carbonera+TV/Palazzo+dei+Trecento,+Treviso,+TV/')
+              .success(function (data, status) {
+                  $scope.jsonBob = JSON.parse(JSON.stringify(data));
+                  fnsuccess($scope.jsonBob, status, 1, $scope.stepBob, $scope.bob);
+              })
+              .error(function (data, status, headers, config) {
+                  $scope.restData = "errore nel ricevimento dati json ";
+              });
+      }
+      else
+      {
+          fnsuccess($scope.jsonBob, status, 1, $scope.stepBob, $scope.bob);
+      }
+      $scope.stepBob +=1;
+
+      //chuck
+
+      $scope.stepChuck = checkStep($scope.jsonChuck, $scope.stepChuck);
+      if($scope.stepChuck == 0) {
+          $http.get('/proxy/gmapsdirections/Dosson+TV/Piazza+dei+Signori,+Treviso,+TV/')
+              .success(function (data, status) {
+                  $scope.jsonChuck = JSON.parse(JSON.stringify(data));
+                  fnsuccess($scope.jsonChuck, status, 1, $scope.stepChuck, $scope.chuck);
+              })
+              .error(function (data, status, headers, config) {
+                  $scope.restData = "errore nel ricevimento dati json ";
+              });
+      }
+      else
+      {
+          fnsuccess($scope.jsonChuck, status, 1, $scope.stepChuck, $scope.chuck);
+      }
+      $scope.stepChuck +=1;
+
   }
-  /*
-  $http.get('/proxy/gmapsdirections/Carbonera+TV/Palazzo+dei+Trecento,+Treviso,+TV/')
-      .success(function(data, status) {
-            fnsuccess(data,status, 2);
-      })
-      .error(function(data, status, headers, config) {
-            $scope.restData = "errore nel ricevimento dati json ";
-      });
 
-  $http.get('/proxy/gmapsdirections/Dosson+TV/Piazza+dei+Signori,+Treviso,+TV/')
-      .success(function(data, status) {
-            fnsuccess(data,status, 3);
-      })
-      .error(function(data, status, headers, config) {
-            $scope.restData = "errore nel ricevimento dati json ";
-      });
-  */
+  $interval($scope.simulation, 1000);
+
   });
