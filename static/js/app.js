96a2644414929cae1942e28b2042a24dbc628d75
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-01-10 01:06:25 +0100

    added average points

--- static/js/app.js
+++ static/js/app.js
@@ -2,63 +2,53 @@ var findparkApp = angular.module('findPark', ['uiGmapgoogle-maps']);
 
 findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval, $timeout) {
   $scope.map = {center: {latitude: 45.4627338, longitude: 9.1777322 }, zoom: 12, bounds: {}};
+
+  // given bounds and center i need to find random points between the fist and the third quarter of the cartesian plane
+  // lat [+90, -90] longitude [+180, -180]
+  // Average point between 2 points, formula based on Talete theorem
+  var firtsQuarterPointX = $scope.map.bounds.northeast.longitude + $scope.map.center.longitude / 2;
+  var firtsQuarterPointY = $scope.map.bounds.northeast.latitude + $scope.map.center.latitude / 2;
+  var thirdQuarterPointX = $scope.map.bounds.southwest.longitude + $scope.map.center.longitude / 2;
+  var thirdQuarterPointY = $scope.map.bounds.southwest.latitude + $scope.map.center.latitude / 2;
+
   $scope.paths = [];
   var obj ={};
   $scope.drivers = [];
   $scope.drivers_details = [];
   var num_drivers = 3;
   var driver_obj = {};
-
         var alice = {
             id: 0,
             coords: {
-            latitude: 45.4627338,
-            longitude: 9.1777322
+                latitude:   ,
+                longitude: 9.1777322
             },
             icon: "http://maps.google.com/intl/en_us/mapfiles/ms/micons/yellow.png",
-            options: { draggable: false },
+            options : {
+                draggable: false,
+                labelContent: "lat: " + $scope.alice.coords.latitude + ' ' + 'lon: ' + $scope.alice.coords.longitude,
+                labelAnchor: "100 0",
+                labelClass: "marker-labels"
+            },
             events: {
-            dragend: function (marker, eventName, args) {
-            $log.log('marker dragend');
-            var lat = marker.getPosition().lat();
-            var lon = marker.getPosition().lng();
-            $log.log(lat);
-            $log.log(lon);
-
-            $scope.alice.options = {
-            draggable: false,
-            labelContent: "lat: " + $scope.alice.coords.latitude + ' ' + 'lon: ' + $scope.alice.coords.longitude,
-            labelAnchor: "100 0",
-            labelClass: "marker-labels"
-            };
             }
-        }};
+        };
 
       var bob = {
-      id: 0,
-      coords: {
-        latitude: 45.4627338,
-        longitude: 9.1777322
-      },
-      options: { draggable: false },
-      icon: "http://maps.google.com/intl/en_us/mapfiles/ms/micons/red.png",
-      events: {
-        dragend: function (marker, eventName, args) {
-          $log.log('marker dragend');
-          var lat = marker.getPosition().lat();
-          var lon = marker.getPosition().lng();
-          $log.log(lat);
-          $log.log(lon);
-
-          $scope.bob.options = {
+          id: 0,
+          coords: {
+            latitude: 45.4627338,
+            longitude: 9.1777322
+          },
+          options : {
             draggable: false,
             labelContent: "lat: " + $scope.bob.coords.latitude + ' ' + 'lon: ' + $scope.bob.coords.longitude,
             labelAnchor: "100 0",
             labelClass: "marker-labels"
-          };
-        }
-      }
-    };
+          },
+          icon : "http://maps.google.com/intl/en_us/mapfiles/ms/micons/red.png",
+          events : {}
+      };
 
     var chuck = {
       id: 0,
@@ -66,23 +56,16 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval, $tim
         latitude: 45.4627338,
         longitude: 9.1777322
       },
-      options: { draggable: false },
-      icon: "http://maps.google.com/intl/en_us/mapfiles/ms/micons/green.png",
-      events: {
-        dragend: function (marker, eventName, args) {
-          $log.log('marker dragend');
-          var lat = marker.getPosition().lat();
-          var lon = marker.getPosition().lng();
-
-          $scope.chuck.options = {
+      options : {
             draggable: false,
             labelContent: "lat: " + $scope.chuck.coords.latitude + ' ' + 'lon: ' + $scope.chuck.coords.longitude,
             labelAnchor: "100 0",
             labelClass: "marker-labels"
-          };
-        }
-      }
+          },
+      icon: "http://maps.google.com/intl/en_us/mapfiles/ms/micons/green.png",
+      events: {}
     };
+
     $scope.drivers.push(alice);
     $scope.drivers.push(bob);
     $scope.drivers.push(chuck);
@@ -91,6 +74,10 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval, $tim
     details.json = {};
     details.step = 0;
     details.latestChange = new Date();
+
+    var point = {'latitude': -90 + 180 * Math.random(), 'longitude' :-180 + 360 * Math.random()};
+    details.start = "Porta+Ticinese,+Piazza+24+Maggio,+20136+Milano";
+    details.end = "Porta+Garibaldi,+Piazza+XXV+Aprile,+Milano,+MI";
 
     $scope.drivers_details.push(details);
     $scope.drivers_details.push(details);
@@ -118,7 +105,7 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval, $tim
     }
 
   function fnsuccess(data, status, id, step, marker, index){
-        var jsonObj = data; //JSON.parse(JSON.stringify(data));
+        var jsonObj = data;
         obj.id = id;
         obj.stroke =  {
             color: '#60' + id + '0FB',
@@ -139,7 +126,14 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval, $tim
         marker.coords.latitude = end_location.latitude;
         marker.coords.longitude = end_location.longitude;
 
-        $interval(function() {$scope.simulation(index);}, 1000);
+        if (step % 4 == 0)
+        {
+            // wait a longer period to the next iteration
+            $timeout(function(){ $scope.simulation(index); }, Math.floor((Math.random() * WaitingTimeMax) + 1) * 10);
+        }
+        else {
+            $timeout(function(){ $scope.simulation(index); }, Math.floor((Math.random() * WaitingTimeMax) + 1));
+        }
   };
 
   checkStep = function(jsonObj, step)
@@ -157,15 +151,14 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval, $tim
     return step;
   };
 
-  var WaitingTimeMax = 1000;
+  var WaitingTimeMax = 10000;
 
   $scope.simulation = function(index) {
       driver = $scope.drivers[index];
       driver_details = $scope.drivers_details[index];
-      driver_details.step = checkStep(driver_details.json, driver_details.step);
-
+      $scope.drivers_details[index].step = checkStep(driver_details.json, driver_details.step);
       if (driver_details.step == 0 || driver_details.json.routes == []) {
-          $http.get('/proxy/gmapsdirections/Porta+Ticinese,+Piazza+24+Maggio,+20136+Milano/Porta+Garibaldi,+Piazza+XXV+Aprile,+Milano,+MI/')
+          $http.get('/proxy/gmapsdirections/' + driver_details.start + '/' + driver_details.end + '/')
               .success(function (data, status) {
                   driver_details.json = JSON.parse(JSON.stringify(data));
                   fnsuccess(driver_details.json, status, 1, driver_details.step, driver, index);
@@ -178,23 +171,29 @@ findparkApp.controller('mapCtrl', function ($scope, $http, $log, $interval, $tim
           fnsuccess(driver_details.json, status, 1, driver_details.step, driver, index);
       }
 
-      driver.step += 1;
-      if (driver.step % 4 == 0)
-      {
-        // wait a longer period
-        $timeout(function(){ $scope.simulation(index); }, Math.floor((Math.random() * WaitingTimeMax) + 1) * 10);
-      }
-      else {
-        $timeout(function(){ $scope.simulation(index); }, Math.floor((Math.random() * WaitingTimeMax) + 1));
-      }
+      $scope.drivers_details[index].step += 1;
+
   };
+
+
+      stopTimeout = 10000;
+      driver = $scope.drivers[0];
+
+      driver_details = $scope.drivers_details[0];
+      $timeout(function(){ $scope.simulation(0); }, 1000);
+
+      driver = $scope.drivers[1];
+
+      driver_details = $scope.drivers_details[1];
+      $timeout(function(){ $scope.simulation(1); }, 1000);
+
+      driver = $scope.drivers[2];
+
+      driver_details = $scope.drivers_details[2];
+      $timeout(function(){ $scope.simulation(2); }, 1000);
 
   for (i = 0; i< num_drivers; i++)
   {
-      stopTimeout = 10000;
-      driver = $scope.drivers[i];
-      driver_details = $scope.drivers_details[i];
-      $timeout(function(){ $scope.simulation(0); }, 1000);
   }
 
   });
