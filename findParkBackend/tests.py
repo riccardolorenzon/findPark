e7a4bd73d2bfc719c21bdceff4034726b2794e4d
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-01-13 22:38:56 +0100

    first test OK

--- findParkBackend/tests.py
+++ findParkBackend/tests.py
@@ -11,5 +11,7 @@ class ParkingSpotsTests(APITestCase):
         Ensure we can create a new account object.
         """
         url = reverse('parkingspot-list')
-        response = self.client.post(url, None, format='json')
+        response = self.client.post(url, {'status': 'open',
+                    'latitude': '0.000',
+                    'longitude': '1.000', 'area': None }, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
