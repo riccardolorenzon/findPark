3a7ab1002b39594e162558ddace98e86e6ae9293
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-01-20 21:46:56 +0100

    added test

--- findParkBackend/tests.py
+++ findParkBackend/tests.py
@@ -6,13 +6,24 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 class ParkingSpotsTests(APITestCase):
-    def test_create_account(self):
+    def test_create_parkingspot(self):
         """
-        Ensure we can create a new account object.
+        ensure you can create a new parking spot and add it to the parking spot list via rest
         """
         url = reverse('parkingspot-list')
         response = self.client.post(url, {'status': 'open',
                     'latitude': '0.000',
                     'longitude': '1.000', 'area': None }, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_update_parkingspot(self):
+        """
+        ensure you can update the status of an existing parking spot via rest
+        """
+        url = reverse('parkingspot-detail')
+        response = self.client.put(url, {''
+
+        }, format = 'json')
+        self.assertEqual(1 == 1)
 
